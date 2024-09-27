5e175c0b89465e09b8e45aa7bf624c9537be5dc7
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-09-27 20:32:50 +0200

    Creation du modèle item

---
+++ db/migrate/20240927175929_create_items.rb
@@ -0,0 +1,29 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :img_url
+      t.json :unit_price_info, default: {
+      price_list: [],
+      median_price: 0,
+      capital_gain: 0,
+      is_worth: false,
+      }
+      t.json :tenth_price_info, default: {
+        price_list: [],
+        median_price: 0,
+        capital_gain: 0,
+        is_worth: false,
+        }
+      t.json :hundred_price, default: {
+        price_list: [],
+        median_price: 0,
+        capital_gain: 0,
+        is_worth: false,
+        }
+      t.string :ressource_type
+
+      t.timestamps
+    end
+  end
+end
