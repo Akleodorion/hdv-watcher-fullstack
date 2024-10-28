ca05d7dfb5eac86261da1d0c41dc009f5e070c61
Christian Bondzie <73429254+Akleodorion@users.noreply.github.com>
2024-10-28 21:25:50 +0100

    Add Select To Index (#10)
    * implémentation du select
    
    * Ajout du Jsonb & de l'implémentationd des selects.
    
    * Refacto du code.
    
    ---------
    
    Co-authored-by: Christian Bondzie <christian@Christians-MacBook-Air.local>

--- db/migrate/20240927175929_create_items.rb
+++ db/migrate/20240927175929_create_items.rb
@@ -3,21 +3,21 @@ class CreateItems < ActiveRecord::Migration[7.1]
     create_table :items do |t|
       t.string :name
       t.string :img_url
-      t.json :unit_price_info, default: {
+      t.jsonb :unit_price_info, default: {
       price_list: [],
       median_price: 0,
       capital_gain: 0,
       current_price: 0,
       is_worth: false,
       }
-      t.json :tenth_price_info, default: {
+      t.jsonb :tenth_price_info, default: {
         price_list: [],
         median_price: 0,
         capital_gain: 0,
         current_price: 0,
         is_worth: false,
         }
-      t.json :hundred_price_info, default: {
+      t.jsonb :hundred_price_info, default: {
         price_list: [],
         median_price: 0,
         capital_gain: 0,
