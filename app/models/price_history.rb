72afe64444caece271ff63a92f207785d9319e36
Christian Bondzie <73429254+Akleodorion@users.noreply.github.com>
2024-10-29 19:51:09 +0100

    Création des callback de bases sur les 3 modèles (#12)
    * Etablissement du callback sur price.
    
    * Etablissement de la méthode d'update des prix sur un PriceHistory
    
    * Etablissement d'un callback sur le modèle Item.
    
    ---------
    
    Co-authored-by: Christian Bondzie <christian@Christians-MacBook-Air.local>

--- app/models/price_history.rb
+++ app/models/price_history.rb
@@ -5,4 +5,21 @@ class PriceHistory < ApplicationRecord
   enum :price_type, { unit: 0, tenth: 1, hundred: 2 }
 
   validates :median_price, :capital_gain, :current_price, :is_worth, :price_type, presence: true
+
+  def update_price_history
+    set_median_price
+    set_current_price
+    set_capital_gain
+    set_is_worth  
+  end
+
+  private
+
+  def set_median_price;end
+
+  def set_current_price;end
+
+  def set_capital_gain;end
+
+  def set_is_worth;end
 end
