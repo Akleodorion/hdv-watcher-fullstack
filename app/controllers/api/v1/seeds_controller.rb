1072ce08cec00ffbd4a09b1db53bf255975fd8d7
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-11-01 11:20:40 +0100

    Modification du endpoint items sur l'api seeds. Création de la view .jb items. Diminution du nombre d'informations envoyé par requête sur le endpoint seeds.

--- app/controllers/api/v1/seeds_controller.rb
+++ app/controllers/api/v1/seeds_controller.rb
@@ -1,8 +1,18 @@
-class API::V1::SeedsController < ApplicationController
+class Api::V1::SeedsController < ApplicationController
   
   def items
+    #renvoie les items, dans une quantité déterminer.
+    index = seeds_params[:index].to_i
+    @items = Item.all.limit(5).offset(index * 5)
   end
 
   def infos
+    # renvoie des informations pour préparer un seeding.
+    # 1. Le nombre d'item.
+  end
+
+
+  def seeds_params
+    params.permit(:index)
   end
 end
