70da5175411ad4706f13af1de0c99bd7dadc5280
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-11-01 12:58:03 +0100

    Retrait des méthodes seeds du items controller, et implémentation du controller seeds.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,15 +12,15 @@ class Api::V1::ItemsController < ApplicationController
       hundred_price: :hundred,
     }
     @items_count = Item.joins(:price_histories)
-                  .where(ressource_type: RessourceTypes.types)
-                  .where(price_histories: { price_type: @price_type_map[@price_type], is_worth: true }).count
+                       .where(ressource_type: RessourceTypes.types)
+                       .where(price_histories: { price_type: @price_type_map[@price_type], is_worth: true }).count
     @items = Item.joins(:price_histories)
-             .where(ressource_type: RessourceTypes.types)
-             .where(price_histories: { price_type: @price_type_map[@price_type], is_worth: true })
-             .select('items.id, items.name, items.img_url, items.ressource_type, price_histories.capital_gain, price_histories.current_price, price_histories.median_price')
-             .order('price_histories.capital_gain DESC')
-             .limit(@selected_batch_size)
-             .offset(batch_index * @selected_batch_size)
+                 .where(ressource_type: RessourceTypes.types)
+                 .where(price_histories: { price_type: @price_type_map[@price_type], is_worth: true })
+                 .select('items.id, items.name, items.img_url, items.ressource_type, price_histories.capital_gain, price_histories.current_price, price_histories.median_price')
+                 .order('price_histories.capital_gain DESC')
+                 .limit(@selected_batch_size)
+                 .offset(batch_index * @selected_batch_size)
   end
 
   def show
@@ -43,23 +43,7 @@ class Api::V1::ItemsController < ApplicationController
     #recoit les informations relative au scrapping.
   end
 
-  def seeds_items
-  batch_index = params[:batch_index].to_i
-
-  @items = Item.all
-  @items = @items.limit(@batch_size)
-  @items = @items.offset(batch_index * @batch_size)
-
-  render json: @items
-  end
-
-  def seeds_info
-    batch_count = (Item.all.count.to_f / @batch_size).ceil
-
-    render json: {
-      batch_count:
-    }
-  end
+  
 
   private
 
@@ -81,10 +65,6 @@ class Api::V1::ItemsController < ApplicationController
 
   def paginated_items(items, batch_size)
     items.limit(batch_size).offset(paginated_items_params[:batch_index].to_i * batch_size)
-  end
-
-  def set_batch_size 
-    @batch_size = 500
   end
 
 
--- app/controllers/api/v1/seeds_controller.rb
+++ app/controllers/api/v1/seeds_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::SeedsController < ApplicationController
   
   def items
-    #renvoie les items, dans une quantité déterminer.
     index = seeds_params[:index].to_i
     @items = Item.all.limit(5).offset(index * 5)
   end
 
   def infos
-    # renvoie des informations pour préparer un seeding.
-    # 1. Le nombre d'item.
+    @items_count = Item.count
   end
 
 
