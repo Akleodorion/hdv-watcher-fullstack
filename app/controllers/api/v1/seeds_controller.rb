e6268486e26b7f2367573a6616c483366117a42b
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-11-03 00:11:31 +0100

    Ajout de la gem activerecords import. Implémentation de la méthode populate.

--- app/controllers/api/v1/seeds_controller.rb
+++ app/controllers/api/v1/seeds_controller.rb
@@ -1,8 +1,31 @@
 class API::V1::SeedsController < ApplicationController
-  
-  def items
+
+  # Ajout d'items en base de donnés.
+  def populate
+    price_to_import = []
+    items_data = populate_params(:items).to_sym
+
+    items_data.each do |item_data|
+      item_created = Item.create!(name: item_data[:name], img_url: item_data[:img_url], ressource_type: item_data[:ressource_type])
+      ph_ids = item_created.price_histories.ids
+
+      item_data[:unit_price].each_with_index do |price, index|
+        date = item_data[:scrap_date][index]
+        price_to_import << Price.new(value: price, date: date, price_history_id: ph_ids[0])
+        price_to_import << Price.new(value: item_data[:tenth_price], date: date, price_history_id: ph_ids[1])
+        price_to_import << Price.new(value: item_data[:hundred_price], date: date, price_history_id: ph_ids[2])
+      end
+    end
+
+    Price.import(price_to_import)    
   end
 
   def infos
+  end
+
+  private
+
+  def populate_params
+    params.permit(:items)
   end
 end
